363d809c76ebeb2de75be3bc1f2ed1c059aafe57
Sweta Rani <swetasr1509@gmail.com>
2021-10-03 23:40:15 +0530

    Updated

--- JS/app.js
+++ JS/app.js
@@ -3,33 +3,6 @@ var inputValue = document.querySelector('.input');
 var image = document.querySelector('.myImage');
 const add = document.querySelector('.add');
 
-function checkLoginState() {
-    FB.getLoginStatus(function(response) {
-        statusChangeCallback(response);
-    });
-}
-
-
-// window.fbAsyncInit = function() {
-//     FB.init({
-//         appId: '{your-app-id}',
-//         cookie: true,
-//         xfbml: true,
-//         version: '{api-version}'
-//     });
-
-//     FB.AppEvents.logPageView();
-
-// };
-
-// (function(d, s, id) {
-//     var js, fjs = d.getElementsByTagName(s)[0];
-//     if (d.getElementById(id)) { return; }
-//     js = d.createElement(s);
-//     js.id = id;
-//     js.src = "https://connect.facebook.net/en_US/sdk.js";
-//     fjs.parentNode.insertBefore(js, fjs);
-// }(document, 'script', 'facebook-jssdk'));
 
 if (window.localStorage.getItem("todos") == undefined) {
     var todos = [];
